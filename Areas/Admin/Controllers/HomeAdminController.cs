187ae5430ac80b5c11fe96005d529e2f933412f8
Hao <hideonbush8405@gmail.com>
2024-05-05 15:23:53 +0700

    update areas

---
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EatWellAssistant.Areas.Admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
